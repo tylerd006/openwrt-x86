4b957e6ea7616a1de3c09085cae6c3442a0bbd76
tylerd006 <48270619+tylerd006@users.noreply.github.com>
2021-10-07 07:22:21 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,9 +14,9 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 # Add a feed source
-# sed -i &#39;$a src-git lienol https://github.com/Lienol/openwrt-package&#39; feeds.conf.default
+sed -i &#39;$a src-git lienol https://github.com/Lienol/openwrt-package&#39; feeds.conf.default
 sed -i &#39;$a src-git hwfuture https://github.com/hongweifuture/openwrt-packages&#39; feeds.conf.default
 sed -i &#39;$a src-git pwdep https://github.com/hongweifuture/pwdep.git&#39; feeds.conf.default
